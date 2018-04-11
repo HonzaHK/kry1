848f03ecd7e8fb6ef5bf23e1642c84c56dca50e3
HonzaHK <zgupie@gmail.com>
2018-04-11 17:09:05 +0200

    FIX

--- solution.py
+++ solution.py
@@ -3,11 +3,11 @@
 
 def getKeystream():
 	file_plaintext = open("in/bis.txt","rb")
-	plaintext = file_plaintext.read(512)
+	plaintext = file_plaintext.read(32)
 	file_plaintext.close()
 
 	file_ciphertext = open("in/bis.txt.enc","rb")
-	ciphertext = file_ciphertext.read(512)
+	ciphertext = file_ciphertext.read(32)
 	file_ciphertext.close()
 
 	keystream = [ plainbyte^cypherbyte for (plainbyte,cypherbyte) in zip(plaintext,ciphertext) ]
@@ -16,7 +16,7 @@ def getKeystream():
 
 
 keystream = getKeystream()
-keystream256 = keystream[:32]
+# keystream256 = keystream[:32]
 
 file_python = open("in/super_cipher.py.enc","rb")
 pythontext = file_python.read(512)
@@ -40,10 +40,15 @@ def step(x):
 	return y
 
 
-xin = int.from_bytes("KRY".encode(),'little')
-kr_cypher = step(xin)
+# a = int.from_bytes(''.join(str(x) for x in keystream).encode(),'little')
+# a=int.from_bytes(plaintext,'little')
+# b=int.from_bytes(ciphertext,'little')
+c=int.from_bytes(keystream,'little')
+keystr = int.from_bytes("KRY{0123456789}".encode(),'little')
+for i in range(N//2):
+	keystr = step(keystr)
 
-print(kr_cypher)
+print(keystr)
 print("---------------------------")
 
 subb = {
@@ -62,39 +67,47 @@ subb = {
 }
 
 
-msb_prev = 1 & kr_cypher>>(255)
-possibles = list(subb[msb_prev].keys())
-paths = [[possibles[0]],[possibles[1]],[possibles[2]],[possibles[3]]]
+y_after_step = c
 
-for i in range(1,255):
-	msb_curr = 1 & kr_cypher>>(255-i)
+for i in range(N//2):
+	msb_prev = 1 & y_after_step>>(255)
+	possibles = list(subb[msb_prev].keys())
+	paths = [[possibles[0]],[possibles[1]],[possibles[2]],[possibles[3]]]
 
-	newPossibles = []
-	for possible in possibles:
-		newPossibles.append(subb[msb_prev][possible][msb_curr])
+	for j in range(1,256):
+		msb_curr = 1 & y_after_step>>(255-j)
 
-	paths[0].append(newPossibles[0])
-	paths[1].append(newPossibles[1])
-	paths[2].append(newPossibles[2])
-	paths[3].append(newPossibles[3])
-	msb_prev = msb_curr
-	possibles = newPossibles
+		newPossibles = []
+		for possible in possibles:
+			newPossibles.append(subb[msb_prev][possible][msb_curr])
+
+		paths[0].append(newPossibles[0])
+		paths[1].append(newPossibles[1])
+		paths[2].append(newPossibles[2])
+		paths[3].append(newPossibles[3])
+		msb_prev = msb_curr
+		possibles = newPossibles
 
 
-#pro kazodu path - prvni prvek beru cely, z kazdeho dalsiho pouze posledni bit
-#jakmile poskladam potencialni vstup, poslu jej do funkce step, abych zjistil, ktery z nich je spravny
-rightPath = None
-for path in paths:
-	appendedPath = path[0] #prvni prvek beru cely (3 bity)
-	for elem in path[1:]:
-		appendedPath = appendedPath<<1 | (elem & 1) #z kazdeho dalsiho vezmu lsb a prilepim ho zprava
+	#pro kazodu path - prvni prvek beru cely, z kazdeho dalsiho pouze posledni bit
+	#jakmile poskladam potencialni vstup, poslu jej do funkce step, abych zjistil, ktery z nich je spravny
+	x_orig = None
+	for path in paths:
+		x_potencial = path[0] & 3#prvni prvek beru cely (3 bity)
+		for elem in path[1:len(path)-1]:
+			x_potencial = x_potencial<<1 | (elem & 1) #z kazdeho dalsiho vezmu lsb a prilepim ho zprava
 
-	if(step(appendedPath)==kr_cypher):
-		rightPath = appendedPath
 
-res = rightPath.to_bytes(33,byteorder='little')
+		if(step(x_potencial)==y_after_step):
+			x_orig = x_potencial
+
+
+	# x_orig = (x_orig>>1) & ~(1>>N)
+	# x_orig = (x_orig>>1) | (x_orig & (0<<N+1)) | (x_orig & (0<<N))
+	# print("step ",i)
+	y_after_step = x_orig
+
+res = x_orig.to_bytes(29,byteorder='little').decode()
+# print("{:b}".format(x_orig))
 print(res)
-
-
-
 
